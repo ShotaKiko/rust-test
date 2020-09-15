b4d6fbd477da85471c5bcf44849440099d47ec98
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-09-15 01:58:45 -0500

    added ability to add and remove todo items

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use web_sys::HtmlInputElement;
 
 use std::mem;
 const ENTER_KEY: u32 = 13;
+use enclose::enc;
 
 //---------------------------------------
 //               Structs
@@ -149,6 +150,7 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 //          View and subViews
 //---------------------------------------
 
+//________________Overall Container View_________________
 fn view(model: &Model) -> impl IntoNodes<Msg> {
     let data = &model.todo_data;
     // div![
@@ -163,11 +165,22 @@ fn view(model: &Model) -> impl IntoNodes<Msg> {
 
     nodes![
         header_view(&data.new_todo_name),
-        //other sub views will follow header
+        if data.todo_list.is_empty() {
+            vec![]
+        } else {
+            vec![
+                content_view(
+                    &data.todo_list,
+                    &data.editing_todo_item,
+                    &model.todo_ref.editing_todo_input,
+                ),
+            ]
+        }
     ]
 }
 
-//__________Header View___________
+//________________Header View_________________
+
 fn header_view(new_todo_name: &str) -> Node<Msg> {
     header![
         C!["header"],
@@ -183,12 +196,61 @@ fn header_view(new_todo_name: &str) -> Node<Msg> {
             IF!(keyboard_event.key_code() == ENTER_KEY => Msg::CreateNewTodoItem)
         }),
         input_ev(Ev::Input, Msg::NewTodoTitleUpdated)
+        ],
+        button![
+            C!["addTodoButton"],
+            ["Add New To Do"],
+            ev(Ev::Click, enc!((new_todo_name) move |_| Msg::CreateNewTodoItem))
         ]
     ]
 }
 
+//________________Content View__________________
 
+fn content_view(
+    todo_list: &IndexMap<TodoItemId, TodoItem>,
+    editing_todo_item: &Option<EditingTodoItem>,
+    editing_todo_input: &ElRef<HtmlInputElement>,
+) -> Node<Msg> {
+    section![
+        C!["contentContainer"],
+        ul![
+            C!["todoList"],
+            todo_list.iter().filter_map(|(todo_item_id, todo_item)| {
+                let show_all = true; //modify for showing filtered list
 
+                IF!(show_all => todo_view(todo_item_id, todo_item, editing_todo_item, editing_todo_input))
+            })
+        ]
+    ]
+}
+
+//________________Todo Item View__________________
+
+fn todo_view(
+    todo_item_id: &TodoItemId,
+    todo_item: &TodoItem,
+    editing_todo_item: &Option<EditingTodoItem>,
+    editing_todo_input: &ElRef<HtmlInputElement>,
+) -> Node<Msg> {
+    li![
+        C![
+            IF!(matches!(editing_todo_item, Some(editing_todo_item) if &editing_todo_item.id == todo_item_id) => "editing"),
+        ],
+        label![
+            ev(
+                Ev::DblClick,
+                enc!((todo_item_id) move |_| Msg::StartTodoEdit(todo_item_id))
+            ),
+            &todo_item.title
+        ],
+        button![
+            C!["removeTodoButton"],
+            ["X"],
+            ev(Ev::Click, enc!((todo_item_id) move |_| Msg::RemoveTodoItem(todo_item_id)))
+        ]
+    ]
+}
 
 //---------------------------------------
 //          Initialization
